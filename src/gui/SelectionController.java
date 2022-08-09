5e8a38909b1c68802469cb01da798ee9c733807e
Hilan-CP <hilan.paganini@gmail.com>
2022-08-09 20:41:22 -0300

    Commit inicial

---
+++ src/gui/SelectionController.java
@@ -0,0 +1,77 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class SelectionController implements Initializable{
+
+    @FXML
+    private Button addBlacklistButton;
+
+    @FXML
+    private Button addPhonesButton;
+
+    @FXML
+    private TextField blacklistTextField;
+
+    @FXML
+    private ListView<?> blacklists;
+
+    @FXML
+    private Button executeButton;
+
+    @FXML
+    private TextField phoneTextField;
+
+    @FXML
+    private Button removeBlacklistButton;
+
+    @FXML
+    private Button searchBlacklistButton;
+
+    @FXML
+    private Button searchPhoneButton;
+
+    @FXML
+    void addBlacklistButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addPhonesButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void executeButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeBlacklistButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchBlacklistButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchPhoneButtonAction(ActionEvent event) {
+
+    }
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module BlacklistWA_JavaFX {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
